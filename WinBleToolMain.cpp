e9a7a9d97c705cf87f7afd845d048240fc663b15
HEYAHONG <2229388563@qq.com>
2022-09-01 16:19:13 +0800

    修复异常处理(出现异常继续处理剩下的服务)

--- WinBleToolMain.cpp
+++ WinBleToolMain.cpp
@@ -72,23 +72,22 @@ void WinBleToolDialog::RefreshBLEDeviceList()
         wxLogMessage(_T("正在枚举BLE设备..."));
         try
         {
-
             BleDeviceEnumerator::enumerate();
-
             for (BleDeviceInfo const *i : BleDeviceEnumerator::getBleDevices())
             {
-                try
-                {
-                    wxLogMessage(_T("\r\n名称: %s\r\nHardwareId: %s\r\nInstanceId: %s"),i->getName(),i->getHardwareId(),i->getInstanceId());
-                    wxLogMessage(_T("正在枚举BLE设备服务..."));
-                    BleDevice bleDevice = BleDevice(i->getInstanceId());
-                    bleDevice.enumerateBleServices();
-                    {
-                        std::stringstream out;
-                        out << "BleGattServices count: " << bleDevice.getBleGattServices().size() << endl;
 
-                        for (unique_ptr<BleGattService> const& s : bleDevice.getBleGattServices())
+                wxLogMessage(_T("\r\n名称: %s\r\nHardwareId: %s\r\nInstanceId: %s"),i->getName(),i->getHardwareId(),i->getInstanceId());
+                wxLogMessage(_T("正在枚举BLE设备服务..."));
+                BleDevice bleDevice = BleDevice(i->getInstanceId());
+                bleDevice.enumerateBleServices();
+                {
+                    std::stringstream out;
+                    out << "BleGattServices count: " << bleDevice.getBleGattServices().size() << endl;
+                    for (unique_ptr<BleGattService> const& s : bleDevice.getBleGattServices())
+                    {
+                        try
                         {
+                            //显示服务信息
                             auto tohex=[](uint32_t num) -> std::string
                             {
                                 char buff[32]={0};
@@ -116,70 +115,67 @@ void WinBleToolDialog::RefreshBLEDeviceList()
                                     << "\t\tIsWritableWithoutResponse:" << +c->getIsWritableWithoutResponse() << endl;
                             }
                         }
-
-                        wxLogMessage("\r\n%s",out.str());
-
+                        catch (BleException const &e)
                         {
-                            auto tohex=[](uint32_t num) -> std::string
-                            {
-                                char buff[32]={0};
-                                sprintf(buff,"%x",num);
-                                return buff;
-                            };
-                            wxLogMessage(_T("正在查找设备信息服务..."));
-                            BleDevice::BleGattServices const &services = bleDevice.getBleGattServices();
-                            {
-                                std::stringstream out;
-                                auto it = find_if(begin(services), end(services), [&](unique_ptr<BleGattService> const& s)
-                                {
-                                    return s->getServiceUuid().Value.ShortUuid == GATT_UUID_DEVICE_INFO_SVC;
-                                });
-
-                                if (it != services.end())
-                                {
-                                    auto service = it->get();
-
-                                    out << "Found Device Information Service: [" << tohex(service->getServiceUuid().Value.ShortUuid) << "]" << endl;
-
-                                    BleGattService::BleGattCharacteristics const& characteristics = service->getBleCharacteristics();
-
-                                    auto readCharacteristicValueAndDisplay=[&](BleGattService::BleGattCharacteristics const &characteristics, USHORT uuid)
-                                    {
-                                        auto cit = find_if(begin(characteristics), end(characteristics), [&](unique_ptr<BleGattCharacteristic> const &c)
-                                        {
-                                            return c->getCharacteristicUuid().Value.ShortUuid == uuid;
-                                        });
-
-                                        if (cit != characteristics.end())
-                                        {
-                                            auto characteristic = cit->get();
-
-                                            BleGattCharacteristicValue value = characteristic->getValue();
-
-                                            out << "\tUUID:" << tohex(uuid) <<"\tValue :" << value.getData() << endl;
-                                        }
-                                    };
-
-                                    readCharacteristicValueAndDisplay(characteristics, GATT_UUID_MANU_NAME);
-
-                                    readCharacteristicValueAndDisplay(characteristics, GATT_UUID_SW_VERSION_STR);
-
-                                    readCharacteristicValueAndDisplay(characteristics, GATT_UUID_MODEL_NUMBER_STR);
-
-                                    wxLogMessage("\r\n%s",out.str());
-                                }
-                            }
-                            wxLogMessage(_T("\t查找设备信息服务完成..."));
+                            wxLogError(e.what());
+                            wxLogError(_T("访问设备服务出错..."));
                         }
-
-                        wxLogMessage(_T("枚举BLE设备服务完成..."));
                     }
+                    wxLogMessage("\r\n%s",out.str());
+                    {
+                        auto tohex=[](uint32_t num) -> std::string
+                        {
+                            char buff[32]={0};
+                            sprintf(buff,"%x",num);
+                            return buff;
+                        };
+                        wxLogMessage(_T("正在查找设备信息服务..."));
+                        BleDevice::BleGattServices const &services = bleDevice.getBleGattServices();
+                        {
+                            std::stringstream out;
+                            auto it = find_if(begin(services), end(services), [&](unique_ptr<BleGattService> const& s)
+                            {
+                                return s->getServiceUuid().Value.ShortUuid == GATT_UUID_DEVICE_INFO_SVC;
+                            });
 
+                            if (it != services.end())
+                            {
+                                auto service = it->get();
+
+                                out << "Found Device Information Service: [" << tohex(service->getServiceUuid().Value.ShortUuid) << "]" << endl;
+
+                                BleGattService::BleGattCharacteristics const& characteristics = service->getBleCharacteristics();
+
+                                auto readCharacteristicValueAndDisplay=[&](BleGattService::BleGattCharacteristics const &characteristics, USHORT uuid)
+                                {
+                                    auto cit = find_if(begin(characteristics), end(characteristics), [&](unique_ptr<BleGattCharacteristic> const &c)
+                                    {
+                                        return c->getCharacteristicUuid().Value.ShortUuid == uuid;
+                                    });
+
+                                    if (cit != characteristics.end())
+                                    {
+                                        auto characteristic = cit->get();
+
+                                        BleGattCharacteristicValue value = characteristic->getValue();
+
+                                        out << "\tUUID:" << tohex(uuid) <<"\tValue :" << value.getData() << endl;
+                                    }
+                                };
+
+                                readCharacteristicValueAndDisplay(characteristics, GATT_UUID_MANU_NAME);
+
+                                readCharacteristicValueAndDisplay(characteristics, GATT_UUID_SW_VERSION_STR);
+
+                                readCharacteristicValueAndDisplay(characteristics, GATT_UUID_MODEL_NUMBER_STR);
+
+                                wxLogMessage("\r\n%s",out.str());
+                            }
+                        }
+                        wxLogMessage(_T("\t查找设备信息服务完成..."));
+                    }
                 }
-                catch(BleException const &e)
-                {
-                    wxLogError(e.what());
-                }
+                wxLogMessage(_T("枚举BLE设备服务完成..."));
 
             }
         }
